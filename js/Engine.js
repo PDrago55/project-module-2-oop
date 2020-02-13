3684b49ca3b2a81960e83b34e422f1dfc8361aef
PDrago55 <drago_paolo@yahoo.ca>
2020-02-13 17:01:20 -0500

    game-test

--- js/Engine.js
+++ js/Engine.js
@@ -1,64 +1,74 @@
-// The engine class will only be instantiated once. It contains all the logic
-// of the game relating to the interactions between the player and the
-// enemy and also relating to how our enemies are created and evolve over time
 class Engine {
-    // The constructor has one parameter. It will refer to the DOM node that we will be adding everything to.
-    // You need to provide the DOM node when you create an instance of the class
-    constructor(theRoot) {
-        // We need the DOM element every time we create a new enemy so we
-        // store a reference to it in a property of the instance.
-        this.root = theRoot;
-        // We create our hamburger.
-        // Please refer to Player.js for more information about what happens when you create a player
-        this.player = new Player(this.root);
-        // Initially, we have no enemies in the game. The enemies property refers to an array
-        // that contains instances of the Enemy class
-        this.enemies = [];
-        // We add the background image to the game
-        addBackground(this.root);
-    }
+  constructor(theRoot) {
+    this.root = theRoot;
+    this.player = new Player(this.root);
+    console.log(this.player);
+    this.enemies = [];
+    addBackground(this.root);
+  }
 
-    // The gameLoop will run every few milliseconds. It does several things
-    //  - Updates the enemy positions
-    //  - Detects a collision between the player and any enemy
-    //  - Removes enemies that are too low from the enemies array
-    gameLoop = () => {
-        // This code is to see how much time, in milliseconds, has elapsed since the last
-        // time this method was called.
-        // (new Date).getTime() evaluates to the number of milliseconds since January 1st, 1970 at midnight.
-        if (this.lastFrame === undefined) this.lastFrame = (new Date).getTime();
-        let timeDiff = (new Date).getTime() - this.lastFrame;
-        this.lastFrame = (new Date).getTime();
-        // We use the number of milliseconds since the last call to gameLoop to update the enemy positions.
-        // Furthermore, if any enemy is below the bottom of our game, its destroyed property will be set. (See Enemy.js)
-        this.enemies.forEach(enemy => {
-            enemy.update(timeDiff);
-        });
-        // We remove all the destroyed enemies from the array referred to by \`this.enemies\`.
-        // We use filter to accomplish this.
-        // Remember: this.enemies only contains instances of the Enemy class.
-        this.enemies = this.enemies.filter(enemy => {
-            return !enemy.destroyed;
-        });
-        // We need to perform the addition of enemies until we have enough enemies.
-        while (this.enemies.length < MAX_ENEMIES) {
-            // We find the next available spot and, using this spot, we create an enemy.
-            // We add this enemy to the enemies array 
-            const spot = nextEnemySpot(this.enemies);
-            this.enemies.push(new Enemy(this.root, spot));
-        }
-        // We check if the player is dead. If he is, we alert the user
-        // and return from the method (Why is the return statement important?)
-        if (this.isPlayerDead()) {
-            window.alert("Game over");
-            return;
-        }
-        // If the player is not dead, then we put a setTimeout to run the gameLoop in 20 milliseconds
-        setTimeout(this.gameLoop, 20);
+  gameLoop = () => {
+    if (this.lastFrame === undefined) this.lastFrame = new Date().getTime();
+    let timeDiff = new Date().getTime() - this.lastFrame;
+    this.lastFrame = new Date().getTime();
+    this.enemies.forEach(enemy => {
+      enemy.update(timeDiff);
+    });
+    this.enemies = this.enemies.filter(enemy => {
+      return !enemy.destroyed;
+    });
+    while (this.enemies.length < MAX_ENEMIES) {
+      const spot = nextEnemySpot(this.enemies);
+      this.enemies.push(new Enemy(this.root, spot));
     }
-    // This method is not implemented correctly, which is why
-    // the burger never dies. In your exercises you will fix this method.
-    isPlayerDead = () => {
-        return false;
+    if (this.isPlayerDead()) {
+      // window.alert("Game over");
+      return;
     }
+    setTimeout(this.gameLoop, 20);
+  };
+
+  isPlayerDead = () => {
+    let x = false;
+    this.enemies.forEach(function(item) {
+      //   console.log(this);
+      if (
+        item.x + ENEMY_WIDTH >= this.player.x &&
+        item.x <= this.player.x + PLAYER_WIDTH &&
+        item.y + ENEMY_HEIGHT >= this.player.y &&
+        item.y <= this.player.y + PLAYER_HEIGHT
+      )
+        console.log("x");
+      x = true;
+    }, this);
+    console.log("x", x);
+    return x;
+  };
 }
+
+//   if (this.player.x === item.x) {
+//     // console.log(this.player, item);
+//     if (this.player.y < item.y) {
+//       console.log("collision");
+//     }
+//   }
+// }, this);
+
+//             console.log(this);
+//             if((item.x + ENEMY_WIDTH) >= (this.player.x) && (item.x) <=
+//             (this.player.x + PLAYER_WIDTH) && (item.y + ENEMY_HEIGHT) >=
+//             (this.player.y) && (item.y) <= (this.player.y + PLAYER_HEIGHT))
+//             console.log(item)
+//             return true;
+//         }, this)
+//     }
+
+// if (catHitbox.x < burgerHitbox.x + burgerHitbox.width &&
+//     catHitbox.x + catHitbox.width > burgerHitbox.x &&
+//     catHitbox.y < burgerHitbox.y + burgerHitbox.height &&
+//     catHitbox.y + catHitbox.height < burgerHitbox.y)
+// alert('you dead')
+// return true;
+// console.log(isPlayerDead)
+// let catHitbox = {x: 10, y: 5, height: 156, width: 75}
+// let burgerHitbox = {x: 'this.x', y: 'y', height: 54, width: 75}
